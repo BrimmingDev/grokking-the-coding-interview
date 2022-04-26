d414226683e9831072e3062eb69c052e60960572
Evan Brim <evanbrim84@gmail.com>
2022-04-25 20:07:48 -0400

    Completed Longest Substring with Distinct Characters (hard)

---
+++ sliding-windows/longest_substring_with_distinct_chars_test.go
@@ -0,0 +1,75 @@
+// Problem Statement#
+// Given a string, find the length of the longest substring, which has all distinct characters.
+
+// Example 1:
+// Input: String="aabccbb"
+// Output: 3
+// Explanation: The longest substring with distinct characters is "abc".
+
+// Example 2:
+// Input: String="abbbb"
+// Output: 2
+// Explanation: The longest substring with distinct characters is "ab".
+
+// Example 3:
+// Input: String="abccde"
+// Output: 3
+// Explanation: Longest substrings with distinct characters are "abc" & "cde".
+
+package slidingwindows
+
+import (
+	"testing"
+
+	"github.com/BrimmingDev/grokking-the-coding-interview/common"
+)
+
+// O(n) time complexity where n is the length of string, O(n) space complexity where size is the length of string
+func LongestDistinctSubstring(str string) int {
+	seen := make(map[string]int)
+	start := 0
+	maxLength := 0
+
+	for end := 0; end < len(str); end++ {
+		rightChar := string(str[end])
+
+		seen[rightChar] += 1
+
+		for seen[rightChar] > 1 {
+			leftChar := string(str[start])
+			seen[leftChar] -= 1
+			start++
+		}
+
+		maxLength = common.MaxInt(maxLength, end-start+1)
+	}
+
+	return maxLength
+}
+
+func TestLongestDistinct_Example1(t *testing.T) {
+	const expected = 3
+	got := LongestDistinctSubstring("aabccbb")
+
+	if got != expected {
+		t.Errorf("Expected %d but received %d", expected, got)
+	}
+}
+
+func TestLongestDistinct_Example2(t *testing.T) {
+	const expected = 2
+	got := LongestDistinctSubstring("abbbb")
+
+	if got != expected {
+		t.Errorf("Expected %d but received %d", expected, got)
+	}
+}
+
+func TestLongestDistinct_Example3(t *testing.T) {
+	const expected = 3
+	got := LongestDistinctSubstring("abccde")
+
+	if got != expected {
+		t.Errorf("Expected %d but received %d", expected, got)
+	}
+}
